5dcf958056bbfd1cfdfeef298902ccd1e354403c
Michael J. Simons <michael@simons.ac>
2017-06-05 22:30:51 +0200

    Überarbeite Format

--- src/main/java/de/springbootbuch/email/Application.java
+++ src/main/java/de/springbootbuch/email/Application.java
@@ -2,6 +2,7 @@ package de.springbootbuch.email;
 
 import static de.springbootbuch.email.PrepareMockSmtpListener.LATCH;
 import java.nio.charset.StandardCharsets;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -30,13 +31,19 @@ public class Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		this.mailSender.send(mimeMessage -> {
-			final MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, StandardCharsets.UTF_8.name());
+			final MimeMessageHelper message = 
+				new MimeMessageHelper(
+					mimeMessage, true, UTF_8.name()
+				);
 			message.setFrom("hallo@springbootbuch.de");
 			message.setTo("leser@springbootbuch.de");
 			message.setSubject("Danke!");
 			message.setText(
 				"Danke, dass Ihr mein Buch lest!",
-				"<html><h1>Hallo!</h1><p>Danke, dass ihr mein Buch lest!</p></html>"
+				  "<html>"
+				+ "<h1>Hallo!</h1>"
+				+ "<p>Danke, dass ihr mein Buch lest!</p>"
+				+ "</html>"
 			);
 		});
 		LATCH.await();

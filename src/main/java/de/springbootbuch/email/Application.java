9243fcb8af21c919d171f830ef30f099e9a68ed1
Michael J. Simons <michael@simons.ac>
2017-06-05 22:09:23 +0200

    Create email example

---
+++ src/main/java/de/springbootbuch/email/Application.java
@@ -0,0 +1,44 @@
+package de.springbootbuch.email;
+
+import static de.springbootbuch.email.PrepareMockSmtpListener.LATCH;
+import java.nio.charset.StandardCharsets;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+/**
+ * Part of springbootbuch.de.
+ *
+ * @author Michael J. Simons
+ * @author @rotnroll666
+ */
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	private final JavaMailSender mailSender;
+
+	public Application(JavaMailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.mailSender.send(mimeMessage -> {
+			final MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, StandardCharsets.UTF_8.name());
+			message.setFrom("hallo@springbootbuch.de");
+			message.setTo("leser@springbootbuch.de");
+			message.setSubject("Danke!");
+			message.setText(
+				"Danke, dass Ihr mein Buch lest!",
+				"<html><h1>Hallo!</h1><p>Danke, dass ihr mein Buch lest!</p></html>"
+			);
+		});
+		LATCH.await();
+	}
+}
---
+++ src/main/java/de/springbootbuch/email/PrepareMockSmtpListener.java
@@ -0,0 +1,112 @@
+package de.springbootbuch.email;
+
+import static de.springbootbuch.email.PrepareMockSmtpListener.LATCH;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.CountDownLatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.subethamail.smtp.MessageContext;
+import org.subethamail.smtp.MessageHandler;
+import org.subethamail.smtp.MessageHandlerFactory;
+import org.subethamail.smtp.RejectException;
+import org.subethamail.smtp.server.SMTPServer;
+
+/**
+ * Part of springbootbuch.de.
+ *
+ * @author Michael J. Simons
+ * @author @rotnroll666
+ */
+public class PrepareMockSmtpListener implements SpringApplicationRunListener {
+
+	public static final CountDownLatch LATCH = new CountDownLatch(1);
+	
+	private final SMTPServer smtpServer;
+
+	public PrepareMockSmtpListener(final SpringApplication springApplication, String[] args) {
+		final MessageHandlerFactory messageHandlerFactory = new MessageHandlerImpl();
+		this.smtpServer = new SMTPServer(messageHandlerFactory);
+		this.smtpServer.setPort(41209);
+	}
+
+	@Override
+	public void starting() {
+	}
+
+	@Override
+	public void environmentPrepared(ConfigurableEnvironment environment) {
+	}
+
+	@Override
+	public void contextPrepared(ConfigurableApplicationContext context) {
+		this.smtpServer.start();
+	}
+
+	@Override
+	public void contextLoaded(ConfigurableApplicationContext context) {
+	}
+
+	@Override
+	public void finished(ConfigurableApplicationContext context, Throwable exception) {
+		this.smtpServer.stop();
+	}
+}
+
+class MessageHandlerImpl implements MessageHandlerFactory {
+
+	static final Logger LOG = LoggerFactory
+		.getLogger(Application.class);
+
+	@Override
+	public MessageHandler create(MessageContext ctx) {
+		return new Handler(ctx);
+	}
+
+	static class Handler implements MessageHandler {
+		MessageContext ctx;
+
+		public Handler(MessageContext ctx) {
+			this.ctx = ctx;
+		}
+
+		@Override
+		public void from(String from) throws RejectException {
+			LOG.info("FROM: {}", from);
+		}
+
+		@Override
+		public void recipient(String recipient) throws RejectException {
+			LOG.info("RECIPIENT: {}", recipient);
+		}
+
+		public void data(InputStream data) throws IOException {
+			LOG.info("DATA:");
+			LOG.info(convertStreamToString(data));
+		}
+
+		@Override
+		public void done() {
+			LATCH.countDown();
+		}
+
+		String convertStreamToString(InputStream is) {
+			final BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+			final StringBuilder sb = new StringBuilder();
+			String line = null;
+			try {
+				while ((line = reader.readLine()) != null) {
+					sb.append(line).append("\n");
+				}
+			} catch (IOException e) {
+			}
+			return sb.toString();
+		}
+	}
+}
